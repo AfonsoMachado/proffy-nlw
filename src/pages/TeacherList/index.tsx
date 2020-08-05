93074dec4e6b9cbf37a3e7f8dc876b223af67b54
Afonso Machado <afonso777@hotmail.com>
2020-08-05 14:36:14 -0300

    form finalizado

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -4,6 +4,7 @@ import './styles.css'
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 
 // import { Container } from './styles';
@@ -15,8 +16,36 @@ function TeacherList() {
         {/* Passado para o props.children */}
         <form id="search-teachers">
 
-          <Input name="subject" label="Matéria" />
-          <Input name="week_day" label="Dia da semana" />
+          <Select
+            name="subject"
+            label="Matéria"
+            // Passando as opções do select
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Educação Física', label: 'Educação Física' },
+              { value: 'Química', label: 'Química' },
+              { value: 'Física', label: 'Física' },
+              { value: 'Geografia', label: 'Geografia' },
+              { value: 'História', label: 'História' },
+              { value: 'Português', label: 'Português' },
+              { value: 'Inglês', label: 'Inglês' },
+            ]}
+          />
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            // Passando as opções do select
+            options={[
+              { value: '0', label: 'Domingo' },
+              { value: '1', label: 'Segunda-feira' },
+              { value: '2', label: 'Terça-feira' },
+              { value: '3', label: 'Quarta-feira' },
+              { value: '4', label: 'Quinta-feira' },
+              { value: '5', label: 'Sexta-feira' },
+              { value: '6', label: 'Sábado' },
+            ]}
+          />
           <Input type="time" name="time" label="Hora" />
 
         </form>
