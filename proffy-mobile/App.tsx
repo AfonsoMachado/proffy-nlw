61b73002f89d6d8ef886cd0aac4cf637cc41f969
Afonso Machado <afonso777@hotmail.com>
2020-08-06 15:26:06 -0300

    criando nova tela give classes

--- proffy-mobile/App.tsx
+++ proffy-mobile/App.tsx
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import Landing from './src/pages/Landing'
 
 // tela de carregamento
 import { AppLoading } from 'expo'
@@ -8,6 +7,7 @@ import { AppLoading } from 'expo'
 // Importando fontes
 import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo'
 import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins'
+import AppStack from './src/routes/AppStack';
 
 export default function App() {
 
@@ -25,13 +25,10 @@ export default function App() {
   } else {
     return (
       <>
-        <Landing />
+        <AppStack />
         <StatusBar style="light" />
       </>
-
     );
-
   }
-
 }
 
