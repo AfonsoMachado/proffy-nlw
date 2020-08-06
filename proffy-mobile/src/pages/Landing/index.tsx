4be0fb3bc4708d4e5d87dbcb30fe40a5ddf291f3
Afonso Machado <afonso777@hotmail.com>
2020-08-06 16:02:40 -0300

    tela dar aulas

--- proffy-mobile/src/pages/Landing/index.tsx
+++ proffy-mobile/src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import styles from './styles';
 
@@ -9,6 +11,14 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 
 function Landing() {
+  // implementando navegação entre telas
+  const { navigate } = useNavigation();
+
+  // Função para navegar para a outra tela
+  function handleNavigateToGiveClassesPage() {
+    navigate('GiveClasses')
+  }
+
   return (
     <View style={styles.container}>
       <Image source={landingImg} style={styles.banner} />
@@ -20,15 +30,15 @@ function Landing() {
 
       <View style={styles.buttonsContainer}>
 
-        <TouchableOpacity style={[styles.button, styles.buttonPrimary]} >
+        <RectButton style={[styles.button, styles.buttonPrimary]} >
           <Image source={studyIcon} />
           <Text style={styles.buttonText} >Estudar</Text>
-        </TouchableOpacity>
+        </RectButton>
 
-        <TouchableOpacity style={[styles.button, styles.buttonSecondary]} >
+        <RectButton style={[styles.button, styles.buttonSecondary]} onPress={handleNavigateToGiveClassesPage} >
           <Image source={giveClassesIcon} />
-          <Text style={styles.buttonText} >Estudar</Text>
-        </TouchableOpacity>
+          <Text style={styles.buttonText} >Dar aulas</Text>
+        </RectButton>
       </View>
 
       <Text style={styles.totalConnections} >
