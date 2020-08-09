23b62426101b78c00c392ea49e1c6273f7d9172c
Afonso Machado <afonso777@hotmail.com>
2020-08-09 15:12:28 -0300

    finalizando listagem dos itens

--- proffy-mobile/src/pages/TeacherList/index.tsx
+++ proffy-mobile/src/pages/TeacherList/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, ScrollView } from 'react-native'
 
 import styles from './styles'
 import PageHeader from '../../components/PageHeader'
@@ -10,7 +10,19 @@ function TeacherList() {
     <View style={styles.container}>
       <PageHeader title="Proffys disponíveis" />
 
-      <TeacherItem />
+      {/* TELA COM SCROLL */}
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
+      
     </View>
   )
 
--- proffy-mobile/src/pages/TeacherList/styles.ts
+++ proffy-mobile/src/pages/TeacherList/styles.ts
@@ -4,6 +4,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f0f0f7'
+  },
+
+  // colocando para sobrepo o cabeçalho
+  teacherList: {
+    marginTop: -60,
   }
 })
 
