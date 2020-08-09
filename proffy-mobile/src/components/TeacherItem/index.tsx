215f5e82b2884a75544b7f48591499c6d564ff11
Afonso Machado <afonso777@hotmail.com>
2020-08-09 14:34:06 -0300

    mais itens na tela de teacherItem

--- proffy-mobile/src/components/TeacherItem/index.tsx
+++ proffy-mobile/src/components/TeacherItem/index.tsx
@@ -1,19 +1,46 @@
 import React from 'react';
 import { View, Image, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import styles from './styles';
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png'
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png'
+import whatsappIcon from '../../assets/images/icons/whatsapp.png'
 
 function TeacherItem() {
   return (
     <View style={styles.container}>
-      <View styles={styles.profile}>
+      <View style={styles.profile}>
         <Image 
-          styles={styles.avatar}
+          style={styles.avatar}
           source={{ uri: 'https://avatars3.githubusercontent.com/u/11397955?s=460&u=e11b0f50547f9a3b9be9e8b076b652f374ff46b8&v=4'}}
         />
         <View style={styles.profileInfo}>
           <Text style={styles.name}>Afonso Machado</Text>
           <Text style={styles.subject}>Matemática</Text>
+        </View>
+      </View>
+
+      <Text style={styles.bio}>
+        Lorem ipsum dolor sit amet consectetur.
+        {'\n'}{'\n'}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente omnis error iure repellat architecto, nemo nostrum aperiam sunt?
+      </Text>
+
+      <View style={styles.footer}>
+        <Text style={styles.price}>
+          Preço/hora {'   '}
+          <Text style={styles.priceValue}>R$ 20,00</Text>
+        </Text>
+
+        <View style={styles.buttonsContainer}>
+          <RectButton style={styles.favoriteButton}>
+            <Image source={heartOutlineIcon}/>
+          </RectButton>
+          <RectButton style={styles.contactButton}>
+            <Image source={whatsappIcon}/>
+            <Text style={styles.contactButtonText}>Entrar em contato</Text>
+          </RectButton>
         </View>
       </View>
     </View>
