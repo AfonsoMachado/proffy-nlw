f26892f13ca866adaeab59730916bd252f047d02
Afonso Machado <afonso777@hotmail.com>
2020-08-26 15:45:44 -0300

    obs

--- proffy-mobile/src/services/api.ts
+++ proffy-mobile/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  // URL indicada no expo com a porta do backend
+  // URL indicada no expo com a porta do backend, para conectar o app mobile ao backend da aplicação
   baseURL: 'http://192.168.15.5:3333'
 })
 
